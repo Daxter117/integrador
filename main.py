08de9da9964d4ec75cecbeafe35b32e97b8e1e3e
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2025-12-08 17:20:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routes.rutas_dispositivos import rutadispositivos
-from routes.rutas_lecturas_brutas import rutalecturas_brutas
+from routes.rutas_lecturas_brutas import rutalecturas
 from routes.rutas_usuarios import rutausuarios
 from routes.rutaas_errores import rutaerrores
 from routes.rutas_extractor import rutaextractores
@@ -10,9 +10,10 @@ app = FastAPI()
 
 app.include_router(rutaerrores)
 app.include_router(rutadispositivos)
-app.include_router(rutalecturas_brutas)
+app.include_router(rutalecturas)
 app.include_router(rutaextractores)
 app.include_router(rutaluces)
 
 app.include_router(rutausuarios)
+
 
