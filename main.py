3e99ace2df3c5fc7851e2bf0fa6e17a786124ac6
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2025-12-09 17:35:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.rutas_dispositivos import rutadispositivos
 from routes.rutas_lecturas_brutas import rutalecturas
 from routes.rutas_usuarios import rutausuarios
@@ -8,12 +10,26 @@ from routes.rutas_luces import rutaluces
 
 app = FastAPI()
 
+# ==================================
+#   C O R S   (NECESARIO PARA WEB)
+# ==================================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes reemplazar con tu dominio si quieres
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ==================================
+#   R O U T E R S
+# ==================================
 app.include_router(rutaerrores)
 app.include_router(rutadispositivos)
 app.include_router(rutalecturas)
 app.include_router(rutaextractores)
 app.include_router(rutaluces)
-
 app.include_router(rutausuarios)
+
 
 
