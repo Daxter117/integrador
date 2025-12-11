b61f8c7262da6005e2b9e82cc384f0b5298d7e61
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2025-12-10 18:47:37 -0700

    Update extractores.py

--- schemas/extractores.py
+++ schemas/extractores.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
 
-class extractores(BaseModel):
+class Extractores(BaseModel):
     id_extractor:int
     estado:int
     modo_func:int
     hora_ini:str
     hora_fin:str
     umbral:float
+
     id_dispositivo:int
