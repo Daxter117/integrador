a98ad383d34a9e9dbd603a264805652aceedde48
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2025-12-10 21:03:37 -0700

    Update extractores.py

--- schemas/extractores.py
+++ schemas/extractores.py
@@ -5,7 +5,7 @@ class Extractores(BaseModel):
     id_extractor: int
     estado: int
     modo_func: int
-    hora_inicio: Optional[str] = "00:00:00"   # aceptar formato HH:MM:SS
+    hora_inicio: Optional[str] = "00:00:00"   # formato HH:MM:SS
     hora_fin: Optional[str] = "00:00:00"
     umbral_co2: float
     id_dispositivo: int
