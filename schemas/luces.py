3c7a5b1f01875939cd3a8ab5898cec0254683233
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2025-12-10 20:37:00 -0700

    Update luces.py

--- schemas/luces.py
+++ schemas/luces.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
-
+from typing import Optional
+ 
 class Luces(BaseModel):
-    id_luz:int
-    estado:int
-    modo:int
-    hora_inicio:str
-    hora_fin:str
-    id_dispositivo:int
+    id_luz: int
+    estado_luz: int
+    modo_func: int
+    hora_inicio: Optional[str] = None   # aceptar "08:00"
+    hora_fin: Optional[str] = None      # aceptar "18:00"
+    id_dispositivo: int
