7f8b9ca5660057363975775fcb9893975b29a920
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2025-12-10 20:25:15 -0700

    Update luces.py

--- schemas/luces.py
+++ schemas/luces.py
@@ -1,11 +1,9 @@
 from pydantic import BaseModel
 
 class Luces(BaseModel):
-    id:int
     id_luz:int
-    estado:float
-    modo:float
+    estado:int
+    modo:int
     hora_inicio:str
     hora_fin:str
-
     id_dispositivo:int
