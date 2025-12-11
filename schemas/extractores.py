5ec9057a08a93da14777680ac6874d6c70b3ef14
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2025-12-10 20:36:40 -0700

    Update extractores.py

--- schemas/extractores.py
+++ schemas/extractores.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
-
+from typing import Optional
+ 
 class Extractores(BaseModel):
-    id_extractor:int
-    estado:int
-    modo_func:int
-    hora_ini:str
-    hora_fin:str
-    umbral:float
-    id_dispositivo:int
+    id_extractor: int
+    estado: int
+    modo_func: int
+    hora_inicio: Optional[str] = None   # aceptar "08:00"
+    hora_fin: Optional[str] = None      # aceptar "18:00"
+    umbral_co2: float
+    id_dispositivo: int
