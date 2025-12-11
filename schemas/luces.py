a6f6a7fb291dc68e123d7ed822a8099a4707c461
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2025-12-10 21:03:55 -0700

    Update luces.py

--- schemas/luces.py
+++ schemas/luces.py
@@ -5,6 +5,6 @@ class Luces(BaseModel):
     id_luz: int
     estado_luz: int
     modo_func: int
-    hora_inicio: Optional[str] = "00:00:00"   # aceptar formato HH:MM:SS
+    hora_inicio: Optional[str] = "00:00:00"   # formato HH:MM:SS
     hora_fin: Optional[str] = "00:00:00"
     id_dispositivo: int
