1dc3d75f7b72ad64fbb55d4d284fa3752cddf441
Daxter117 <144505418+Daxter117@users.noreply.github.com>
2026-03-26 15:35:49 -0700

    Update db.py

--- database/db.py
+++ database/db.py
@@ -2,19 +2,16 @@ import psycopg2
 
 try:
     connexion = psycopg2.connect(
-        # Hostname actualizado de tu última captura
+        
         host="dpg-d6o7li450q8c73amejdg-a.virginia-postgres.render.com",
-        # Base de datos actualizada
         database="oxigarden_pv2x",
         user="usuarios",
-        # Contraseña actualizada
         password="5e4DiAATcMg3P0JZ7MlHVvzrKrytrmOy",
         port=5432,
         sslmode="require"
     )
     print("Conexión exitosa a Render PostgreSQL")
     
-    # Opcional: Cerrar la conexión
     connexion.close()
 
 except Exception as err:
